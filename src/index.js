5560d01b50d2b50aa2f927739036b66969e44510
Arthur-codeX <daniel.karanja239@gmail.com>
2022-03-25 20:04:22 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let input = event.target.querySelector("input").value;
+    console.log(input);
+    fetch("http://localhost:3000/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // LOG: (3) [{…}, {…}, {…}]
+      });
+  });
+};
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
